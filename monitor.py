e6cd3525081f56b3655f3796d5baec6d4c72e5ad
YvanKamaro <Jeanyvankamaro@gmail.com>
2026-07-17 01:52:49 +0300

    Add files via upload

---
+++ monitor.py
@@ -0,0 +1,60 @@
+#Read and store server information  from the user into servers list.
+import os
+from datetime import datetime
+def data():
+    servers= []
+    print("\nEnter server details(e.g Mail01 online/offline/manage)")
+    print("When finished enter q\n")
+    while True:
+        user=input(">")
+        if user.lower()=="q":
+            break
+        valid_status = ["online", "offline", "manage"]
+        if any(status in user.strip() for status in valid_status) and user.strip() not in servers:
+            servers.append(user)
+        else: 
+            print("Please enter a valid server name and statut")
+    
+    return servers
+    
+# Categorizes Servers into 3 lists
+def categorize(servers):
+	online=[]
+	offline=[]
+	manage=[]
+	for n in servers:
+		if "online" in n:
+			online.append(n)
+		elif "offline" in n:
+			offline.append(n)
+		elif "manage" in n:
+			manage.append(n)
+	return online,offline,manage
+	
+# Writes each list to its own file
+def write(online,offline,manage):
+		timestamp=datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+		if online:
+		    with open("online.txt","a") as f:
+		        f.write("\n".join(online)+"-"+timestamp+"\n")
+		if offline:
+		    with open("offline.txt","a") as f:
+		        f.write("\n".join(offline)+"-"+timestamp+"\n")
+		if manage:
+		    with open("manage.txt","a") as f:
+		    	f.write("\n".join(manage)+"-" +timestamp+"\n") 
+	    		
+#Summary
+
+def summary(online,offline,manage):
+		return f"\n=======SERVER STATUS REPORT========	\nOnline:  \t{len(online)} server(s)\nOffline: \t {len(offline)} server(s)\nManage: \t {len(manage)} server(s)\n=====================\nServers written respectively to online.txt,offline.txt, manage.txt\n"
+
+if __name__ == "__main__":
+    print("======================")
+    print("SERVER STATUS MONITOR")
+    print("======================\n")
+    servers=data()
+    print(f"\n➤{len(servers)} Servers.\n")
+    online,offline,manage=categorize(servers)
+    write(online,offline,manage)
+    print("\n",summary(online,offline,manage))
